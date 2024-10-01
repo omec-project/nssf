98dd78581cd161190f818d5bd12bbd7b2b481080
Dario Faccin <35623244+dariofaccin@users.noreply.github.com>
2024-10-01 18:48:52 +0200

    feat: add NS selections metric (#124)
    * feat: add NSSF metrics counters
    
    Signed-off-by: Dario Faccin <dario.faccin@canonical.com>
    
    * chore: remove unused metrics
    
    Signed-off-by: Dario Faccin <dario.faccin@canonical.com>
    
    ---------
    
    Signed-off-by: Dario Faccin <dario.faccin@canonical.com>

--- metrics/telemetry.go
+++ metrics/telemetry.go
@@ -11,8 +11,40 @@ import (
 	"net/http"
 
 	"github.com/omec-project/nssf/logger"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+// NssfStats captures NSSF stats
+type NssfStats struct {
+	nssfNsSelections *prometheus.CounterVec
+}
+
+var nssfStats *NssfStats
+
+func initNssfStats() *NssfStats {
+	return &NssfStats{
+		nssfNsSelections: prometheus.NewCounterVec(prometheus.CounterOpts{
+			Name: "nssf_ns_selections",
+			Help: "Counter of total NS selection queries",
+		}, []string{"target_nf_type", "nf_id", "result"}),
+	}
+}
+
+func (ps *NssfStats) register() error {
+	if err := prometheus.Register(ps.nssfNsSelections); err != nil {
+		return err
+	}
+	return nil
+}
+
+func init() {
+	nssfStats = initNssfStats()
+
+	if err := nssfStats.register(); err != nil {
+		logger.InitLog.Errorln("NSSF Stats register failed")
+	}
+}
 
 // InitMetrics initialises NSSF metrics
 func InitMetrics() {
@@ -20,4 +52,9 @@ func InitMetrics() {
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		logger.InitLog.Errorf("Could not open metrics port: %v", err)
 	}
+}
+
+// IncrementNssfNsSelectionsStats increments number of total NS selection queries
+func IncrementNssfNsSelectionsStats(targetNfType, nfId, result string) {
+	nssfStats.nssfNsSelections.WithLabelValues(targetNfType, nfId, result).Inc()
 }
