9f1b70ccb14301c927bf107dc5ba865d5a8a8ee6
Patricia Reinoso <patricia.reinoso@canonical.com>
2025-07-09 16:26:13 +0200

    feat: use new NF config system
    Signed-off-by: Patricia Reinoso <patricia.reinoso@canonical.com>

--- service/init.go
+++ service/init.go
@@ -12,6 +12,7 @@ package service
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"os"
 	"os/exec"
@@ -19,17 +20,15 @@ import (
 	"path/filepath"
 	"sync"
 	"syscall"
-	"time"
 
-	grpcClient "github.com/omec-project/config5g/proto/client"
-	protos "github.com/omec-project/config5g/proto/sdcoreConfig"
-	"github.com/omec-project/nssf/consumer"
-	"github.com/omec-project/nssf/context"
+	nssfContext "github.com/omec-project/nssf/context"
 	"github.com/omec-project/nssf/factory"
 	"github.com/omec-project/nssf/logger"
 	"github.com/omec-project/nssf/metrics"
+	"github.com/omec-project/nssf/nfregistration"
 	"github.com/omec-project/nssf/nssaiavailability"
 	"github.com/omec-project/nssf/nsselection"
+	"github.com/omec-project/nssf/polling"
 	"github.com/omec-project/openapi/models"
 	"github.com/omec-project/util/http2_util"
 	utilLogger "github.com/omec-project/util/logger"
@@ -56,11 +55,6 @@ var nssfCLi = []cli.Flag{
 		Required: true,
 	},
 }
-
-var (
-	KeepAliveTimer      *time.Timer
-	KeepAliveTimerMutex sync.Mutex
-)
 
 func (*NSSF) GetCliCmd() (flags []cli.Flag) {
 	return nssfCLi
@@ -89,72 +83,9 @@ func (nssf *NSSF) Initialize(c *cli.Command) error {
 
 	factory.NssfConfig.CfgLocation = absPath
 
-	if os.Getenv("MANAGED_BY_CONFIG_POD") == "true" {
-		logger.InitLog.Infoln("MANAGED_BY_CONFIG_POD is true")
-		go manageGrpcClient(factory.NssfConfig.Configuration.WebuiUri)
-	} else {
-		go func() {
-			logger.CfgLog.Infoln("use helm chart config")
-			factory.ConfigPodTrigger <- true
-		}()
-	}
 	factory.Configured = true
-	context.InitNssfContext()
+	nssfContext.InitNssfContext()
 	return nil
-}
-
-// manageGrpcClient connects the config pod GRPC server and subscribes the config changes.
-// Then it updates NSSF configuration.
-func manageGrpcClient(webuiUri string) {
-	var configChannel chan *protos.NetworkSliceResponse
-	var client grpcClient.ConfClient
-	var stream protos.ConfigService_NetworkSliceSubscribeClient
-	var err error
-	count := 0
-	for {
-		if client != nil {
-			if client.CheckGrpcConnectivity() != "READY" {
-				time.Sleep(time.Second * 30)
-				count++
-				if count > 5 {
-					err = client.GetConfigClientConn().Close()
-					if err != nil {
-						logger.InitLog.Infof("failing ConfigClient is not closed properly: %+v", err)
-					}
-					client = nil
-					count = 0
-				}
-				logger.InitLog.Infoln("checking the connectivity readiness")
-				continue
-			}
-
-			if stream == nil {
-				stream, err = client.SubscribeToConfigServer()
-				if err != nil {
-					logger.InitLog.Infof("failing SubscribeToConfigServer: %+v", err)
-					continue
-				}
-			}
-
-			if configChannel == nil {
-				configChannel = client.PublishOnConfigChange(true, stream)
-				logger.InitLog.Infoln("PublishOnConfigChange is triggered")
-				go factory.NssfConfig.UpdateConfig(configChannel)
-				logger.InitLog.Infoln("NSSF updateConfig is triggered")
-			}
-
-			time.Sleep(time.Second * 5) // Fixes (avoids) 100% CPU utilization
-		} else {
-			client, err = grpcClient.ConnectToConfigServer(webuiUri)
-			stream = nil
-			configChannel = nil
-			logger.InitLog.Infoln("connecting to config server")
-			if err != nil {
-				logger.InitLog.Errorf("%+v", err)
-			}
-			continue
-		}
-	}
 }
 
 func (nssf *NSSF) setLogLevel() {
@@ -203,18 +134,29 @@ func (nssf *NSSF) Start() {
 
 	go metrics.InitMetrics()
 
-	self := context.NSSF_Self()
+	self := nssfContext.NSSF_Self()
 	addr := fmt.Sprintf("%s:%d", self.BindingIPv4, self.SBIPort)
+
+	plmnConfigChan := make(chan []models.PlmnId, 1)
+	ctx, cancelServices := context.WithCancel(context.Background())
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		polling.StartPollingService(ctx, factory.NssfConfig.Configuration.WebuiUri, plmnConfigChan)
+	}()
+	go func() {
+		defer wg.Done()
+		nfregistration.StartNfRegistrationService(ctx, plmnConfigChan)
+	}()
 
 	signalChannel := make(chan os.Signal, 1)
 	signal.Notify(signalChannel, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-signalChannel
-		nssf.Terminate()
+		nssf.Terminate(cancelServices, &wg)
 		os.Exit(0)
 	}()
-
-	go nssf.registerNF()
 
 	sslLog := filepath.Dir(factory.NssfConfig.CfgLocation) + "/sslkey.log"
 	server, err := http2_util.NewServer(addr, sslLog, router)
@@ -289,119 +231,10 @@ func (nssf *NSSF) Exec(c *cli.Command) error {
 	return err
 }
 
-func (nssf *NSSF) Terminate() {
+func (nssf *NSSF) Terminate(cancelServices context.CancelFunc, wg *sync.WaitGroup) {
 	logger.InitLog.Infoln("terminating NSSF")
-	// deregister with NRF
-	problemDetails, err := consumer.SendDeregisterNFInstance()
-	if problemDetails != nil {
-		logger.InitLog.Errorf("deregister NF instance Failed Problem[%+v]", problemDetails)
-	} else if err != nil {
-		logger.InitLog.Errorf("deregister NF instance error[%+v]", err)
-	} else {
-		logger.InitLog.Infoln("deregister from NRF successfully")
-	}
-
+	cancelServices()
+	nfregistration.DeregisterNF()
+	wg.Wait()
 	logger.InitLog.Infoln("NSSF terminated")
-}
-
-func (nssf *NSSF) StartKeepAliveTimer(nfProfile models.NfProfile) {
-	KeepAliveTimerMutex.Lock()
-	defer KeepAliveTimerMutex.Unlock()
-	nssf.StopKeepAliveTimer()
-	if nfProfile.HeartBeatTimer == 0 {
-		nfProfile.HeartBeatTimer = 60
-	}
-	logger.InitLog.Infof("started KeepAlive Timer: %v sec", nfProfile.HeartBeatTimer)
-	// AfterFunc starts timer and waits for KeepAliveTimer to elapse and then calls nssf.UpdateNF function
-	KeepAliveTimer = time.AfterFunc(time.Duration(nfProfile.HeartBeatTimer)*time.Second, nssf.UpdateNF)
-}
-
-func (nssf *NSSF) StopKeepAliveTimer() {
-	if KeepAliveTimer != nil {
-		logger.InitLog.Infoln("stopped KeepAlive Timer")
-		KeepAliveTimer.Stop()
-		KeepAliveTimer = nil
-	}
-}
-
-func (nssf *NSSF) BuildAndSendRegisterNFInstance() (models.NfProfile, error) {
-	self := context.NSSF_Self()
-	profile, err := consumer.BuildNFProfile(self)
-	if err != nil {
-		logger.InitLog.Errorf("build NSSF profile error: %v", err)
-		return profile, err
-	}
-	logger.InitLog.Infof("NSSF Profile Registering to NRF: %v", profile)
-	// Indefinite attempt to register until success
-	profile, _, self.NfId, err = consumer.SendRegisterNFInstance(self.NrfUri, self.NfId, profile)
-	return profile, err
-}
-
-// UpdateNF is the callback function, this is called when keepalivetimer elapsed
-func (nssf *NSSF) UpdateNF() {
-	KeepAliveTimerMutex.Lock()
-	defer KeepAliveTimerMutex.Unlock()
-	if KeepAliveTimer == nil {
-		logger.InitLog.Warnln("keepAlive timer has been stopped")
-		return
-	}
-	// setting default value 60 sec
-	var heartBeatTimer int32 = 60
-	pitem := models.PatchItem{
-		Op:    "replace",
-		Path:  "/nfStatus",
-		Value: "REGISTERED",
-	}
-	var patchItem []models.PatchItem
-	patchItem = append(patchItem, pitem)
-	nfProfile, problemDetails, err := consumer.SendUpdateNFInstance(patchItem)
-	if problemDetails != nil {
-		logger.InitLog.Errorf("NSSF update to NRF ProblemDetails[%v]", problemDetails)
-		// 5xx response from NRF, 404 Not Found, 400 Bad Request
-		if (problemDetails.Status >= 500 && problemDetails.Status <= 599) ||
-			problemDetails.Status == 404 || problemDetails.Status == 400 {
-			// register with NRF full profile
-			nfProfile, err = nssf.BuildAndSendRegisterNFInstance()
-			if err != nil {
-				logger.InitLog.Errorf("NSSF update to NRF error[%s]", err.Error())
-			}
-		}
-	} else if err != nil {
-		logger.InitLog.Errorf("NSSF update to NRF error[%s]", err.Error())
-		nfProfile, err = nssf.BuildAndSendRegisterNFInstance()
-		if err != nil {
-			logger.InitLog.Errorf("NSSF update to NRF error[%s]", err.Error())
-		}
-	}
-
-	if nfProfile.HeartBeatTimer != 0 {
-		// use hearbeattimer value with received timer value from NRF
-		heartBeatTimer = nfProfile.HeartBeatTimer
-	}
-	logger.InitLog.Debugf("restarted KeepAlive timer: %v sec", heartBeatTimer)
-	// restart timer with received HeartBeatTimer value
-	KeepAliveTimer = time.AfterFunc(time.Duration(heartBeatTimer)*time.Second, nssf.UpdateNF)
-}
-
-func (nssf *NSSF) registerNF() {
-	for msg := range factory.ConfigPodTrigger {
-		logger.InitLog.Infof("minimum configuration from config pod available %v", msg)
-		self := context.NSSF_Self()
-		profile, err := consumer.BuildNFProfile(self)
-		if err != nil {
-			logger.InitLog.Errorln("build profile failed")
-		}
-
-		var newNrfUri string
-		var prof models.NfProfile
-		// send registration with updated PLMN Ids.
-		prof, newNrfUri, self.NfId, err = consumer.SendRegisterNFInstance(self.NrfUri, profile.NfInstanceId, profile)
-		if err == nil {
-			nssf.StartKeepAliveTimer(prof)
-			logger.CfgLog.Infoln("sent register NFInstance with updated profile")
-			self.NrfUri = newNrfUri
-		} else {
-			logger.CfgLog.Errorf("send register NFInstance error[%s]", err.Error())
-		}
-	}
 }
